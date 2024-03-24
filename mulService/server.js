0e98ac4123ae3a705a05fc5deeac62fb6a8d14e3
FaroqAlkatheri <153449802+FaroqAlkatheri@users.noreply.github.com>
2024-03-24 18:40:07 +0000

    final

---
+++ mulService/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World, from gcloud GCP Cloud Run');
+    });
+
+app.post('/', (req, res) =>{
+    const body = req.body;
+    if (typeof body.X === 'undefined' || typeof body.Y === 'undefined') {
+        res.status(400).send('Bad Request: Missing X or Y in the request body');
+        return;
+      }
+    
+    const sum = body.X * body.Y;
+
+    const responseObject = {
+        X: body.X,
+        Y: body.Y,
+        Result: sum
+    };
+
+    res.status(200).json(responseObject);
+});
+
+app.listen(8080, () => {
+    console.log('Server is up on 8080');
+    }
+);
