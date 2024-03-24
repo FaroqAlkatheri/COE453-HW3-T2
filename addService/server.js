25d3e8c9b6552c39e6622c5cd9aba7ecc0126f76
FaroqAlkatheri <153449802+FaroqAlkatheri@users.noreply.github.com>
2024-03-24 18:36:00 +0000

    addCommit

--- addService/server.js
+++ addService/server.js
@@ -1,31 +1,30 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
-app.post('/', (req, res) => {
-  // Ensure the request body contains X and Y
-  const body = req.body;
-  if (typeof body.X === 'undefined' || typeof body.Y === 'undefined') {
-    res.status(400).send('Bad Request: Missing X or Y in the request body');
-    return;
-  }
+app.get('/', (req, res) => {
+    res.send('Hello World, from gcloud GCP Cloud Run');
+    });
 
-  // Calculate the sum of X and Y
-  const sum = body.X + body.Y;
+app.post('/', (req, res) =>{
+    const body = req.body;
+    if (typeof body.X === 'undefined' || typeof body.Y === 'undefined') {
+        res.status(400).send('Bad Request: Missing X or Y in the request body');
+        return;
+      }
+    
+    const sum = body.X + body.Y;
 
-  // Construct the response object
-  const responseObject = {
-    X: body.X,
-    Y: body.Y,
-    Result: sum
-  };
+    const responseObject = {
+        X: body.X,
+        Y: body.Y,
+        Result: sum
+    };
 
-  // Send the response as JSON
-  res.status(200).json(responseObject);
+    res.status(200).json(responseObject);
+});
 
-    }
-);
-
-app.listen(3000, () => {
-    console.log('Server is up on 3000');
+app.listen(8080, () => {
+    console.log('Server is up on 8080');
     }
 );
